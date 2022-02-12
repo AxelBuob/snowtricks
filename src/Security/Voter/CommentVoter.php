7ff3a357a42f64e223e36cda71c75f6ac42be48e
Axel <buobaxel@gmail.com>
2022-02-12 10:06:18 +0100

    hotfix: security Fixes #24

---
+++ src/Security/Voter/CommentVoter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Exception;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\Comment;
+
+class CommentVoter extends Voter
+{
+    protected function supports(string $attribute, $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, ['EDIT'])
+            && $subject instanceof \App\Entity\Comment;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+        if (!$subject instanceof Comment) {
+            throw new  Exception('Wrong type somehow passed!');
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'POST_EDIT':
+                return $user === $subject->getUser();
+        }
+
+        return false;
+    }
+}
